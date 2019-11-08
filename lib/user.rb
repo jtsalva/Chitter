61d3f0f7390bd682a13f17369fa2c96ad2651932
jtsalva <tim@jtsalva.dev>
2019-11-08 12:55:44 +0000

    add user database methods

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,68 @@
-class User
-	attr_reader :username, :name, :email
+require_relative 'store'
 
-	def initialize(username:, name:, email:)
-		@username = username
-		@name = name
-		@email = email
-	end
+class User
+  COLLECTION = 'user'
+
+  attr_reader :username, :name, :email
+
+  def initialize(username:, name:, email:)
+    @username = username
+    @name = name
+    @email = email
+  end
+
+  def self.all
+    all = []
+    Store.all(COLLECTION) do |user|
+      all << User.new(
+        username: user[:username],
+        name: user[:name],
+        email: user[:email]
+      )
+    end
+
+    all
+  end
+
+  def self.get(username)
+    user = Store.get(COLLECTION, ref: username)
+    if user.exists?
+      User.new(username: user[:username], name: user[:name], email: user[:email])
+    else
+      nil
+    end
+  end
+
+  def self.update(user)
+    set(user)
+  end
+
+  def self.add(user)
+    set(user)
+  end
+
+  def self.delete(user)
+    ref = if user.is_a? User
+            user.username
+          else
+            # expecting a username
+            user
+          end
+
+    Store.delete(COLLECTION, ref: ref)
+  end
+
+  def self.set(user)
+    Store.set(
+        COLLECTION,
+        ref: user.username,
+        username: user.username,
+        name: user.name,
+        email: user.email
+    )
+  end
+
+  def self.clear
+    Store.delete(COLLECTION)
+  end
 end
