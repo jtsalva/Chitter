22ebf98ba0cb5231ee418737ab126ca934eee53e
jtsalva <tim@jtsalva.dev>
2019-11-08 12:34:34 +0000

    add user database tests

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,4 +1,6 @@
 require 'user'
+require 'config'
+require 'store'
 
 describe User do
   subject { User.new(username: username, name: name, email: email) }
@@ -17,6 +19,53 @@ describe User do
 
     it 'has an email' do
       expect(subject.email).to eq email
+    end
+  end
+
+  describe 'database actions' do
+    before(:all) do
+      config = Config.new('/Users/jaetimothysalva/projects/chitter/config.json')
+      config.init_test_credentials
+      Store.init config.test_project_id
+    end
+
+    before(:each) do
+      User.clear
+    end
+
+    it 'starts with an empty collection' do
+      expect(User.all.length).to eq 0
+    end
+
+    it 'can add users' do
+      User.add(subject)
+      expect(User.get(subject.username)).to have_attributes(
+        username: subject.username,
+        name: subject.name,
+        email: subject.email
+      )
+    end
+
+    it 'can update users' do
+      User.add(subject)
+      updated_user = User.new(
+        username: subject.username,
+        name: 'updated name',
+        email: subject.email
+      )
+      User.update(updated_user)
+
+      expect(User.get(subject.username)).to have_attributes(
+        username: updated_user.username,
+        name: updated_user.name,
+        email: updated_user.email
+      )
+    end
+
+    it 'can delete users' do
+      User.add(subject)
+      User.delete(subject)
+      expect(User.get(subject.username)).to eq nil
     end
   end
 end
