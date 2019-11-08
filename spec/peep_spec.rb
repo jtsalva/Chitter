1b7a1fbb0d9056afc8fc349fbc50576ba31a5be4
jtsalva <tim@jtsalva.dev>
2019-11-08 12:57:53 +0000

    add peep database tests

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,10 +1,12 @@
 require 'peep'
+require 'config'
+require 'store'
 
 describe Peep do
   subject { Peep.new(text: text, author: author, created_at: created_at) }
   let(:text) { 'lol' }
   let(:author) { 'jtsalva' }
-  let(:created_at) { 'utc_timestamp' }
+  let(:created_at) { Time.now.utc }
 
   describe 'properties' do
     it 'has text' do
@@ -17,6 +19,54 @@ describe Peep do
 
     it 'records timestamp of creation' do
       expect(subject.created_at).to eq created_at
+    end
+  end
+
+  describe 'database actions' do
+    before(:all) do
+      config = Config.new('/Users/jaetimothysalva/projects/chitter/config.json')
+      config.init_test_credentials
+      Store.init config.test_project_id
+    end
+
+    before(:each) do
+      Peep.clear
+    end
+
+    it 'starts with an empty collection' do
+      expect(Peep.all.length).to eq 0
+    end
+
+    it 'can add peeps' do
+      id = Peep.add(subject)
+      expect(Peep.get(id)).to have_attributes(
+        text: subject.text,
+        author: subject.author,
+        created_at: subject.created_at
+      )
+    end
+
+    it 'can update peeps' do
+      id = Peep.add(subject)
+      updated_peep = Peep.new(
+       text: 'updated text',
+       author: subject.author,
+       created_at: subject.created_at,
+       id: id
+      )
+      Peep.update(updated_peep)
+
+      expect(Peep.get(id)).to have_attributes(
+        text: updated_peep.text,
+        author: updated_peep.author,
+        created_at: updated_peep.created_at
+      )
+    end
+
+    it 'can delete peeps' do
+      id = Peep.add(subject)
+      Peep.delete(subject)
+      expect(Peep.get(id)).to eq nil
     end
   end
 end
